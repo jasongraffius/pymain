9ea6cc2ba04fff2be5e550c22a388ba3d0f82c6d
Jason Graffius <jasongraffius@gmail.com>
2017-03-25 10:29:44 -0400

    Can handle positional-only arguments

--- pymain.py
+++ pymain.py
@@ -1,7 +1,23 @@
 
 from typing import Callable
+from functools import wraps
+
+import inspect
+import sys
 
 
-# Decorator
 def pymain(main: Callable[..., None]) -> Callable[..., None]:
-    return main
+    signature = inspect.signature(main)
+
+    @wraps(main)
+    def wrapper(*args, **kwargs):
+        if args or kwargs:
+            main(*args, **kwargs)
+        else:
+            args = list()
+            for p, a in zip(signature.parameters.values(), sys.argv[1:]):
+                args.append(p.annotation(a))
+
+            main(*args)
+
+    return wrapper
