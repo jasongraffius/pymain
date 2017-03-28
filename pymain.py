ea70b3fc38f18634e1d6cc3999a5e73eb8605ddf
Jason Graffius <jasongraffius@gmail.com>
2017-03-28 19:23:20 -0400

    Adding empty check for expanding functionality

--- pymain.py
+++ pymain.py
@@ -1,9 +1,15 @@
-
-from typing import Callable
+from typing import Callable, Union
 from functools import wraps
 
 import inspect
 import sys
+
+_Param = inspect.Parameter
+_Sig = inspect.Parameter
+
+
+def _is_empty(src: Union[_Param, _Sig], val: Union[type, None]) -> bool:
+    return val == type(src).empty
 
 
 def pymain(main: Callable[..., None]) -> Callable[..., None]:
