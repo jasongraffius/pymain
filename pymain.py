b7d38ec070bd104162b1329f1eb354b9f7a5f470
Jason Graffius <jasongraffius@gmail.com>
2017-04-05 21:35:25 -0400

    Added aliases

--- pymain.py
+++ pymain.py
@@ -3,7 +3,10 @@ from functools import wraps
 
 import argparse
 import inspect
-import sys
+
+# To reduce probability of collision, the name of the module is included.
+# Additionally, a syntactically invalid attribute string is used ("!!")
+ALIAS_ATTR = '_!!_' + __name__ + '_aliases'
 
 _Param = inspect.Parameter
 _Sig = inspect.Parameter
@@ -13,6 +16,35 @@ MainFunc = Callable[..., None]
 
 def _is_empty(src: Union[_Param, _Sig], val: Union[type, None]) -> bool:
     return val == type(src).empty
+
+
+def alias(original: str, new: str) -> Callable[[MainFunc], MainFunc]:
+    """Allows aliasing options, useful for short names.
+
+    Used as a decorator, this function returns a function decorator which
+    returns a wrapper, intended to be used for the main function. It adds
+    an alias map to main, to be used in argument parsing.
+    """
+
+    def decorator(main: MainFunc) -> MainFunc:
+        # Retrieve the alias map, or create a new one
+        try:
+            attrs = getattr(main, ALIAS_ATTR)
+        except AttributeError:
+            attrs = dict()
+            setattr(main, ALIAS_ATTR, attrs)
+
+        # Retrieve the list of aliases, or create a new one
+        try:
+            aliases = attrs[original]
+        except KeyError:
+            aliases = []
+            attrs[original] = aliases
+
+        aliases.append(new)
+        return main
+
+    return decorator
 
 
 def pymain(main: MainFunc) -> MainFunc:
@@ -33,12 +65,22 @@ def pymain(main: MainFunc) -> MainFunc:
             optional.append(p)
 
     parser = argparse.ArgumentParser()
+    aliases = getattr(main, ALIAS_ATTR, None)  # type: dict
 
     for p in required:
         parser.add_argument(p.name, type=p.annotation)
     for p in extended + optional:
-        prefix = '--' if len(p.name) > 1 else '-'
-        parser.add_argument(prefix + p.name, default=p.default, type=p.annotation)
+        prefixer = lambda n: '--' + n if len(n) > 1 else '-' + n
+
+        if aliases is not None:
+            alias_list  = aliases.get(p.name, [])
+            alias_list.append(p.name)
+
+            flags = list(map(prefixer, alias_list))
+        else:
+            flags = [prefixer(p.name)]
+
+        parser.add_argument(*flags, default=p.default, type=p.annotation)
 
     @wraps(main)
     def wrapper(*args, **kwargs):
